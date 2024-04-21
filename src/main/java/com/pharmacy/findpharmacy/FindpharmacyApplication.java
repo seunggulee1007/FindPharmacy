4c460aaf28885e73555f2472248685b13523dd05
이승구 <leesg107@naver.com>
2024-04-21 20:10:00 +0900

    initial commit

---
+++ src/main/java/com/pharmacy/findpharmacy/FindpharmacyApplication.java
@@ -0,0 +1,13 @@
+package com.pharmacy.findpharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindpharmacyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindpharmacyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pharmacy/findpharmacy/FindpharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pharmacy.findpharmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindpharmacyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
