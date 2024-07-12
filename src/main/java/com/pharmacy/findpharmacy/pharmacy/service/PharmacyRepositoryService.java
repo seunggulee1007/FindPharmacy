8e0ba27c20ec96641346cb0c9201a534993a3452
이승구 <leesg107@naver.com>
2024-07-12 14:18:26 +0900

    feat: 주소 변경 api 개발

---
+++ src/main/java/com/pharmacy/findpharmacy/pharmacy/service/PharmacyRepositoryService.java
@@ -0,0 +1,29 @@
+package com.pharmacy.findpharmacy.pharmacy.service;
+
+import com.pharmacy.findpharmacy.pharmacy.entity.Pharmacy;
+import com.pharmacy.findpharmacy.pharmacy.repository.PharmacyRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class PharmacyRepositoryService {
+
+    private final PharmacyRepository pharmacyRepository;
+
+    @Transactional
+    public void updateAddress(Long id, String address) {
+        Pharmacy pharmacy = pharmacyRepository.findById(id).orElseThrow(() -> new IllegalStateException("해당 약국이 존재하지 않습니다."));
+
+        pharmacy.changePharmacyAddress(address);
+
+    }
+
+    public void updateAddressWithoutTransaction(Long id, String address) {
+        Pharmacy pharmacy = pharmacyRepository.findById(id).orElseThrow(() -> new IllegalStateException("해당 약국이 존재하지 않습니다."));
+
+        pharmacy.changePharmacyAddress(address);
+    }
+
+}
